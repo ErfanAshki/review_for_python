f4db0519736532646922475f83369ee3b3723df6
erfan ashki <erfanbayern111@gmail.com>
2025-01-15 00:58:30 +0330

    append - pop methods

--- python.py
+++ python.py
@@ -1,18 +1,14 @@
 my_list = [5, 'ugly', '66', 54, 'xx', -8]
 your_list = ['you', 100]
 
-my_list[0] = 50
+my_list.pop()
+your_list.pop(1)
+my_list.pop(-3)
 
-print(my_list + your_list)
-                           
-print(my_list[3])              
-print(my_list[-1])                   
-print(my_list[-5:-2])                  
-print(my_list[4: ])                         
-print(my_list[-5: ])                            
-print(my_list[ :3])                    
-print(my_list[:])                             
-print(my_list[0:6:2])             
+your_list.append('bob')
+your_list.append('sfangi')
+my_list.append(45)
 
-print(len(my_list))
-print(len(your_list))
+
+print(your_list)
+print(my_list)
