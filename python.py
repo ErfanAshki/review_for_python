6a83795fcc149385deb3cce3dc2ecc654b42fff2
erfan ashki <erfanbayern111@gmail.com>
2025-01-15 01:20:08 +0330

    sort - reverse methods

--- python.py
+++ python.py
@@ -1,13 +1,17 @@
-my_list = [5, 'ugly', '66', 54, 'xx', -8]
-your_list = ['you', 100]
+my_list = [5, -7 ,66, 54, -8]
+your_list = [46, 100, 5]
+c = my_list.reverse()
 
-my_list.pop()
-your_list.pop(1)
-my_list.pop(-3)
+my_list.reverse()
+your_list.reverse()
 
-your_list.append('bob')
-your_list.append('sfangi')
-my_list.append(45)
+print(c)
+print(my_list)
+print(your_list)
+
+
+my_list.sort()
+your_list.sort()
 
 
 print(your_list)
