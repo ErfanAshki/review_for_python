6ec78e13cd6d1e3f1fb59ea54a4190ced64ee1b9
erfan ashki <erfanbayern111@gmail.com>
2025-01-14 16:40:17 +0330

    upper - lower - split methods

--- python.py
+++ python.py
@@ -1,15 +1,7 @@
-word1 = 'imaginative'
-word2 = 'and'
-word3 = 'imagination'
-word4 = 'my name is \n erfan'
-word5 = 'it\'s the good book'
-word6 = 'semi \tcircle'
-word7 = 'I want back slash \\'
+my_string = 'I wanna To learn python'
 
-print(word1+' '+ word2+' '+word3)
-print(word3*3)
-print(word2 + 'sss' * 5)
-print(word4)
-print(word5)
-print(word6)
-print(word7)
+print(my_string.lower())
+print(my_string.upper())
+print(my_string.split())
+print(my_string.split('a'))
+print(my_string.split('n'))
