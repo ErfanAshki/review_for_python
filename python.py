709e10fff8822eb4f290585f16204e16364b100b
erfan ashki <erfanbayern111@gmail.com>
2025-01-15 15:28:43 +0330

    and - or - not

--- python.py
+++ python.py
@@ -1,10 +1,15 @@
-my_list = [0, 5, -7]
+print(25>=20 and 10<100 and 40==40)
+print(25>=20 and 10<100 and 40==400)
 
-# my_list.append(10)
-# my_list.append(11)
+print()
+print(5>=20 or 10<0 or 40==40)
+print(5>=20 or 10<0 or 40!=40)
 
-# my_list+= [10,11]
+a = 100
 
-my_list.extend([10,11])
+print()
+print(a>100)
+print(not a>100)
 
-print(my_list)
+print()
+print((((10>5 and 'hi'=='hii') or (12<=6 and 12>10)) and 100==100) or 10>1)
