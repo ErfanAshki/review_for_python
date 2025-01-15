a254724e987a6c5efaedd8572310f75b5d7dd8bf
erfan ashki <erfanbayern111@gmail.com>
2025-01-15 13:38:06 +0330

    string formating & f string

--- python.py
+++ python.py
@@ -1,17 +1,11 @@
-my_int = 10
-my_float = 11.5
-my_string = 'fs'
-my_list = [1,'dd']
-my_tuple = (5,55)
-my_dict = {'k': 'v'}
-my_boolean = False
-my_set = ('dd', 88)
 
-print(type(my_int))
-print(type(my_float))
-print(type(my_string))
-print(type(my_list))
-print(type(my_tuple))
-print(type(my_dict))
-print(type(my_boolean))
-print(type(my_set))
+print("first variable is :  {} - second is : {} - third is {}".format('int', 'string', 'list'))
+print()
+print("fourth variable is : {fourth} - fifth is : {fifth} - sixth is {sixth}".format(fourth='float', fifth='tuple', sixth='dict'))
+
+name = 'erfan'
+age = 25 
+job = 'trader'
+
+print()
+print(f"my name is {name} and {age} years old and i am {job}")
