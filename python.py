2228864d52848cf11577372c8a511ba163faf5c7
erfan ashki <erfanbayern111@gmail.com>
2025-01-15 14:09:40 +0330

    input - int function

--- python.py
+++ python.py
@@ -1,11 +1,9 @@
+user_name = input('Enter your username : ')
+age = int(input('Enter your age : '))
+email = input('Enter your email : ')
 
-print("first variable is :  {} - second is : {} - third is {}".format('int', 'string', 'list'))
-print()
-print("fourth variable is : {fourth} - fifth is : {fifth} - sixth is {sixth}".format(fourth='float', fifth='tuple', sixth='dict'))
 
-name = 'erfan'
-age = 25 
-job = 'trader'
+print(f"user_name = {user_name} -- age = {age} -- email = {email}")
 
-print()
-print(f"my name is {name} and {age} years old and i am {job}")
+print(type(age))
+print(age + 10)
