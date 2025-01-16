6c894c30e4b638788ad237a2daab9ed4d54dca6a
erfan ashki <erfanbayern111@gmail.com>
2025-01-16 12:46:08 +0330

    paper scissors rock game

--- python.py
+++ python.py
@@ -1,10 +1,22 @@
-print(1 in [1,5])
-print('goo' in 'good book')
-print('a' in [1, 'aa', 'bank'])
-print('x' in 'exnovin')
+import random
 
-print()
 
-print(5 not in (4,9,10))
-print('when' not in 'when I go to ...')
-print('slm' not in 'salam')
+choices = ['s', 'p', 'r']
+ai_choice = random.choice(choices) 
+user_choice = input('select from rock , paper , scissor (r, p, s) : ')
+
+if user_choice in choices :
+    print(f'user_choice is {user_choice} and ai_choice is {ai_choice}')
+
+    if user_choice == ai_choice:
+        print('draw')
+
+    elif (user_choice == 's' and ai_choice == 'p') or (user_choice == 'p' and ai_choice == 'r') or (user_choice == 'r' and ai_choice == 's'):
+        print('you win')
+        
+    else:
+        print('lost')
+else:
+    print('invalid input')
+    
+        
