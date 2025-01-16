8c3349513a79b538a6666bc59b83781cf7375c19
erfan ashki <erfanbayern111@gmail.com>
2025-01-16 12:00:33 +0330

    if - elif - else

--- python.py
+++ python.py
@@ -1,15 +1,13 @@
-print(25>=20 and 10<100 and 40==40)
-print(25>=20 and 10<100 and 40==400)
+score = 20
 
-print()
-print(5>=20 or 10<0 or 40==40)
-print(5>=20 or 10<0 or 40!=40)
-
-a = 100
-
-print()
-print(a>100)
-print(not a>100)
-
-print()
-print((((10>5 and 'hi'=='hii') or (12<=6 and 12>10)) and 100==100) or 10>1)
+if score == 20:
+    print('perfect')
+elif score >=15 and score < 20:
+    print('good')
+elif score >=10 and score < 15:
+    print('acceptable')
+elif score >=7 and score <10:
+    print('bad')
+else:
+    print('fail')
+    
