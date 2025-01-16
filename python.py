1b783679021b285e33a9f70b5f26ab0d6984dd89
erfan ashki <erfanbayern111@gmail.com>
2025-01-16 14:22:36 +0330

    examples for for (dict)

--- python.py
+++ python.py
@@ -1,27 +1,20 @@
-# numbers = [1,5,4,54,25,10,22,11]
+person = {
+    'first_name': 'ali',
+    'last_name': 'mamadi',
+    'age': 22,
+    'job': 'trader'
+}
 
-# for num in numbers:
-#     if num % 2 == 0:
-#         print(num, 'even')
-#     elif num % 2 == 1:
-#         print(num, 'odd')
+for data in  person.keys():
+    print(data)
 
-my_str = 'I sending my resume to a lot companies'       
+print()
 
-count_e = 0
-count_m = 0
+for data in  person.values():
+    print(data)
 
-for letter in my_str:
-    if letter == 'e':
-        print('I found e')
-        count_e += 1
-    elif letter == 'm':
-        print('I found m')
-        count_m += 1
-    else:
-        print(letter)
+print()
 
-print()        
-print(f"number of e = {count_e}")
-print(f"number of m = {count_m}")
+for data in  person.items():
+    print(data)
 
