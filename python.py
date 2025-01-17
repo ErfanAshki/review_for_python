57194715dceab00720ad3c6352bae5d2f5196a5e
erfan ashki <erfanbayern111@gmail.com>
2025-01-17 23:42:19 +0330

    example for get number from user and calc Avg with while

--- python.py
+++ python.py
@@ -4,8 +4,9 @@ number_of_numbers = int(input('How many numbers do want to enter ?'))
 
 numbers_list = list()
 
-for _ in range(number_of_numbers):
+while number_of_numbers > 0:
     user_number = int(input('Enter a number for Avg : '))
     numbers_list.append(user_number)
-    
+    number_of_numbers -= 1    
+        
 print(sum(numbers_list) / len(numbers_list))
