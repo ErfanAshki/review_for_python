b160197ffffb92a3ed9fc74294d5e62a380e6865
erfan ashki <erfanbayern111@gmail.com>
2025-01-17 23:39:20 +0330

    example for get number from user and calc Avg with for & range

--- python.py
+++ python.py
@@ -1,5 +1,11 @@
-print(range(1,5))
-print(list(range(1,5)))
-print()
-print(zip([1,2],['a','b']))
-print(list(zip([1,2],['a','b'])))
+print('Welcome to this app')
+
+number_of_numbers = int(input('How many numbers do want to enter ?'))
+
+numbers_list = list()
+
+for _ in range(number_of_numbers):
+    user_number = int(input('Enter a number for Avg : '))
+    numbers_list.append(user_number)
+    
+print(sum(numbers_list) / len(numbers_list))
