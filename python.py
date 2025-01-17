2d449bd84d2bda0480f8a5b91c4099e0fb26583f
erfan ashki <erfanbayern111@gmail.com>
2025-01-17 13:55:58 +0330

    point for print zip and range

--- python.py
+++ python.py
@@ -1,12 +1,5 @@
-first_names = ['john', 'sara', 'hadi', 'mahtab']
-last_names = ['smith', 'babaki']
-age = [10,22,19]
-
-for item in zip(first_names,last_names,age):
-    print(item)
-
-for first,last,age  in zip(first_names,last_names,age):
-    print(age)
-    print(last, first)
-
-    
+print(range(1,5))
+print(list(range(1,5)))
+print()
+print(zip([1,2],['a','b']))
+print(list(zip([1,2],['a','b'])))
