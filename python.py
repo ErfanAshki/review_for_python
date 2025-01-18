1e337c51471b217b4703e263ccad8f2f4640a959
erfan ashki <erfanbayern111@gmail.com>
2025-01-18 15:11:54 +0330

    goal ya poch game

--- python.py
+++ python.py
@@ -1,20 +1,39 @@
-print('determine prime number')
+import random
+
+print('gol ya poch game')
+
+cups = int(input('How many cups do you want ? ' ))
+chances = int(input('How many chances you want to have ? ' ))
+
+LINE_LENGTH = 10 
+
+print()
+print('-'*LINE_LENGTH)
+
+ai_goal = random.randint(1,cups)
+word = 's'
 
 
-while True:
-    prime = True
-
-    user_number = int(input('Enter your number : '))
+for _ in range(chances):
+    if chances - _ == 1:
+        word = ''
     
-    for _ in range(2,user_number):
-        if user_number % _ == 0:
-            prime =False
-            break
-        
-    if prime == True:
-        print('number is prime')
+    print(f"{chances - _} chance{word} left")
+    user_goal = int(input(f'select from {[1,cups]} : '))
+    print()
+    
+    
+    if user_goal == ai_goal :
+        print('Right answer , congratulation')
+        break
     else:
-        print(f'number is not prime because {user_number} % {_}')
+        print('Wrong answer')
         
     print()
-
+    print('-'*LINE_LENGTH)
+    
+if user_goal == ai_goal :
+    print('You win , thanks for your time')    
+else:
+    print(f'You lost because the right answer is {ai_goal}')
+    print('Try again ? :)')
