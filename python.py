ecc9231139cfcf2e0014f46cb573e068f1035ced
erfan ashki <erfanbayern111@gmail.com>
2025-01-18 13:17:38 +0330

    example for rock scissor paper game with for loop and range

--- python.py
+++ python.py
@@ -1,12 +1,45 @@
-print('Welcome to this app')
+import random
 
-number_of_numbers = int(input('How many numbers do want to enter ?'))
+print('Welcome to paper scissor rock game')
 
-numbers_list = list()
+num_of_rounds = int(input('how many round do you want to play ? '))
+choices = ['p', 's', 'r']
 
-while number_of_numbers > 0:
-    user_number = int(input('Enter a number for Avg : '))
-    numbers_list.append(user_number)
-    number_of_numbers -= 1    
+user_point = 0
+ai_point = 0 
+
+
+for _ in range(num_of_rounds):
+    ai_choice = random.choice(choices)
+    user_choice = input('Select from paper scissor rock (p, s, r) : ')
+    print('\n')
+
+    if user_choice in choices:
         
-print(sum(numbers_list) / len(numbers_list))
+        if user_choice == ai_choice:
+            print(f"your choice is {user_choice} and ai choice is {ai_choice}")
+            print('draw')
+        elif (user_choice == 'p' and ai_choice == 'r') or (user_choice == 's' and ai_choice == 'p') or (user_choice == 'r' and ai_choice == 's'):
+            print(f"your choice is {user_choice} and ai choice is {ai_choice}")
+            print('user +1')
+            user_point += 1 
+        else:
+            print(f"your choice is {user_choice} and ai choice is {ai_choice}")
+            print('ai + 1')
+            ai_point += 1
+    else:
+        print('Invalid input , please select from (p, s, r)')
+    
+    
+    print(f"user point = {user_point}  --  ai point = {ai_point}")
+    print()
+    print('------------------')
+
+if user_point > ai_point : 
+    print('You win the game')
+elif user_point == ai_point :
+    print('Same points!!!')
+else:
+    print('You lost , want try again ? :)')
+    
+print("Have a good day")
