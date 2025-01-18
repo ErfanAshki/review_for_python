944703a9735d588f7c500d569cc9629a69e9bf42
erfan ashki <erfanbayern111@gmail.com>
2025-01-18 13:33:29 +0330

    example for rock scissor paper game with while True and round number is five

--- python.py
+++ python.py
@@ -2,20 +2,20 @@ import random
 
 print('Welcome to paper scissor rock game')
 
-num_of_rounds = int(input('how many round do you want to play ? '))
+# num_of_rounds = int(input('how many round do you want to play ? '))
 choices = ['p', 's', 'r']
 
 user_point = 0
 ai_point = 0 
 
 
-while num_of_rounds > 0:
+while True:
     ai_choice = random.choice(choices)
     user_choice = input('Select from paper scissor rock (p, s, r) : ')
     print()
 
     if user_choice in choices:
-        num_of_rounds -= 1
+        # num_of_rounds -= 1
         
         if user_choice == ai_choice:
             print(f"your choice is {user_choice} and ai choice is {ai_choice}")
@@ -35,12 +35,21 @@ while num_of_rounds > 0:
     print(f"user point = {user_point}  --  ai point = {ai_point}")
     print()
     print('------------------')
-
-if user_point > ai_point : 
-    print('You win the game')
-elif user_point == ai_point :
-    print('Same points!!!')
-else:
-    print('You lost , want try again ? :)')
     
+    if user_choice == 5 : 
+        print('You win the game')
+        break
+    elif ai_point == 5:
+        print('You lost , want try again ? :)') 
+        break      
+
 print("Have a good day")
+
+
+# if user_point > ai_point : 
+#     print('You win the game')
+# elif user_point == ai_point :
+#     print('Same points!!!')
+# else:
+#     print('You lost , want try again ? :)')
+    
