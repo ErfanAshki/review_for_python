b8754a8e356bb37a6be6b84500eb498928dd6178
erfan ashki <erfanbayern111@gmail.com>
2025-01-18 15:51:41 +0330

    cryptography password part 1

--- python.py
+++ python.py
@@ -1,39 +1,10 @@
-import random
-
-print('gol ya poch game')
-
-cups = int(input('How many cups do you want ? ' ))
-chances = int(input('How many chances you want to have ? ' ))
-
-LINE_LENGTH = 10 
-
-print()
-print('-'*LINE_LENGTH)
-
-ai_goal = random.randint(1,cups)
-word = 's'
+my_password = 'erfpass55'
+my_list = []
 
 
-for _ in range(chances):
-    if chances - _ == 1:
-        word = ''
+for i in my_password:
+    my_list.append(str(ord(i) * 2 + 20))
     
-    print(f"{chances - _} chance{word} left")
-    user_goal = int(input(f'select from {[1,cups]} : '))
-    print()
+print(','.join(my_list))
+print(type(','.join(my_list)))
     
-    
-    if user_goal == ai_goal :
-        print('Right answer , congratulation')
-        break
-    else:
-        print('Wrong answer')
-        
-    print()
-    print('-'*LINE_LENGTH)
-    
-if user_goal == ai_goal :
-    print('You win , thanks for your time')    
-else:
-    print(f'You lost because the right answer is {ai_goal}')
-    print('Try again ? :)')
