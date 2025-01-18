ff311dd701abd7e558792ac3007746f8382a09bb
erfan ashki <erfanbayern111@gmail.com>
2025-01-18 14:23:52 +0330

    multiply table

--- python.py
+++ python.py
@@ -1,17 +1,6 @@
-# for i in range(4):
-#     for j in range(3):
-#         print(i,j)
-        
-numbers = [[1,2,8], [44,10,6], [10,2], [1,6,55,40]]
-
-for item in numbers:
-    for num in item:
-        print(num)
-
-
-
-# for num in numbers:
-#     print(num)
-#     print(num[1])
-#     print(num[2: ])
+for i in range(1,10):
+    for j in range(1,10):
+        print(f"{i*j:5}", end=' ')
+    print()
     
+ط
