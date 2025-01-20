9022fc4ee7c1b4bf4e413cb0e594dd356e05d1f4
erfan ashki <erfanbayern111@gmail.com>
2025-01-20 16:26:45 +0330

    use *args in function

--- python.py
+++ python.py
@@ -1,24 +1,10 @@
-def is_even(num):
-    if num % 2 == 0:
-        return True
+def sum_numbers(*args):
+    result = 0
 
-def is_odd(num):
-    if num % 2 == 1:
-        return True    
+    for num in args:
+        result += num
     
-def separate_even_odd_numbers(list):
-    even_nums = []
-    odd_nums = []
-    
-    for num in list:
-        if is_even(num):
-            even_nums.append(num)
-        elif is_odd(num):
-            odd_nums.append(num)
-            
-    return even_nums , odd_nums
+    return result
 
 
-numbers_list = [1,5,3,4,6,52,69,63,65,96,30]
-
-print(separate_even_odd_numbers(numbers_list))
+print(sum_numbers(1,5,8,6,4,2,10,2,5,9,1))
