793548cf29e6536ea02633ccdb8ea466c3e50b97
erfan ashki <erfanbayern111@gmail.com>
2025-01-20 16:41:28 +0330

    use *args and **kwargs in function

--- python.py
+++ python.py
@@ -1,6 +1,5 @@
-def some_keys(**kwargs):    
-    return kwargs
+def some_keys(*args , **kwargs):    
+    return kwargs , args
 
 
-print(some_keys(a=1, b='agile', ss='nimble', q=10))
-
+print(some_keys(25, 'swift', a=1, b='agile', ss='nimble', q=10))
