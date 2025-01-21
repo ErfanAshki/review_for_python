b311f823b1f71ec3c7ad92c352d6dfea7fe1bac0
erfan ashki <erfanbayern111@gmail.com>
2025-01-21 14:56:45 +0330

    guess word game with functions

--- python.py
+++ python.py
@@ -1,12 +1,55 @@
-a = 2 
+import random 
 
-def some_function(num):
-    global a
-    a = a + num
-    return a
+list_of_word = ['hi', 'bye', 'sun', 'sky', 'moon', 'fire', 'water'] 
 
-print(a)
-print(some_function(5))
-print(a)
-print(some_function(5))
-print(a)
+
+def start_game():
+    print('Welcome to this game')
+    print('Hope you win')
+    print()
+    print('-' * 15)
+
+
+def get_input():
+    
+    while True:
+        user_choice = input("Enter your guess : ") 
+
+        if user_choice.isalpha():
+            return user_choice
+        else:
+            print('Your guess is wrong please enter a correct guess .')
+
+
+def valid_input():
+    while True:
+        user_input = get_input()
+        
+        if user_input in list_of_word:
+            return user_input
+        else:
+            print('Invalid input')
+        
+
+def run_game(rounds):
+    start_game()
+    ai_input = random.choice(list_of_word)
+    print(f"number of guess you have : {rounds}")
+    
+    for i in range(rounds):
+        user_input = valid_input()
+        
+        if user_input == ai_input:
+            print('Right answer , Well done')
+            return
+        else:
+            print('Wrong answer')
+            print(f"number of guess you have : {rounds -1 -i}")
+        print('-' * 15)
+    
+    print('You lost')
+    print(f"The right guess is {ai_input} .")
+    
+
+run_game(3)
+
