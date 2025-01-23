406c728fe275fe3e8cda77183f90d3b87f444d85
erfan ashki <erfanbayern111@gmail.com>
2025-01-23 23:34:38 +0330

    implement max_min_sum_len function

--- python.py
+++ python.py
@@ -1,12 +1,10 @@
-def factorial(number):
-    if number == 1:
-        return 1
-    return number * factorial(number - 1)
+def max_min_sum_len(*args, list_of_numbers):
+    list_of_numbers.append(args)
+    
+    return max(args) , min(args) , sum(args) , len(args)
 
 
-print(factorial(1))
-print(factorial(2))
-print(factorial(3))
-print(factorial(4))
-print(factorial(5))
-print(factorial(6))
+list_of_numbers = []
+
+print(max_min_sum_len(1,2,3,5,8,5,88,2,663, list_of_numbers=list_of_numbers))
+print(list_of_numbers)
