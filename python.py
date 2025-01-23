34c19fa3652d694a344fa32eeb63e477b0e611be
erfan ashki <erfanbayern111@gmail.com>
2025-01-23 21:31:41 +0330

    fix logic of list variables when using function with copy() method way 2

--- python.py
+++ python.py
@@ -1,10 +1,12 @@
 def custom(num_list):
-    num_list.append(100)
-    return num_list
+    copy_of_num_list = num_list.copy()
+    
+    copy_of_num_list.append(100)
+    return copy_of_num_list
     
 numbers = [1,2,3]
 
 print(numbers)
-print(custom(numbers.copy()))
+print(custom(numbers))
 print(numbers)
 
