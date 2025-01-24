b97f6c54612557961de4832b255d3e64c1d2a7cf
erfan ashki <erfanbayern111@gmail.com>
2025-01-24 13:52:33 +0330

    implement make diamond function

--- python.py
+++ python.py
@@ -1,24 +1,33 @@
-def divisors(num):
-    divisors_list = []
+def make_space (number_of_star , num) : 
+    number_of_space = int((num - number_of_star) / 2) 
+    print(number_of_space * ' ' , end= '') 
+    print(number_of_star * '*' , end= '') 
+    print(number_of_space * ' ')  
+
+
+
+
+
+def make_diamond (num) :
+    print()  
+    for i in range(1,num+1,2) :
+        make_space(i,num)   
     
-    for i in range(1,num):
-        if num % i == 0:
-            divisors_list.append(i)
-    
-    return divisors_list
+    for i in range(1,num,2) :
+        make_space(num-i-1 , num)    
 
-print(divisors(10))
-print(divisors(12))
-print(divisors(81))
+    # for i in range (num) :
+    #     if i < num / 2 : 
+    #         print(i * 2 + 1) 
+    #     else : 
+    #         print((num - i) * 2 - 1 )  
+      
+user_number = int(input("Enter a number : ")) 
+
+if user_number == 0 : 
+    print('Invalid input , please enter a correct number')
+elif user_number % 2 == 0 : 
+    user_number += 1 
 
 
-def is_perfect(num):
-    divisors_list = divisors(num)
-    
-    if sum(divisors_list) == num:
-        return True
-    return False
-
-print(is_perfect(496))
-print(is_perfect(8128))
-print(is_perfect(81))
+make_diamond(user_number) 
