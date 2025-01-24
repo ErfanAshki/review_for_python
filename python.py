8c37d1c4a78a00b629b6a8ace8d45f4454c34fef
erfan ashki <erfanbayern111@gmail.com>
2025-01-24 13:49:05 +0330

    implement is_perfect function

--- python.py
+++ python.py
@@ -1,7 +1,7 @@
 def divisors(num):
     divisors_list = []
     
-    for i in range(1,num+1):
+    for i in range(1,num):
         if num % i == 0:
             divisors_list.append(i)
     
@@ -10,3 +10,15 @@ def divisors(num):
 print(divisors(10))
 print(divisors(12))
 print(divisors(81))
+
+
+def is_perfect(num):
+    divisors_list = divisors(num)
+    
+    if sum(divisors_list) == num:
+        return True
+    return False
+
+print(is_perfect(496))
+print(is_perfect(8128))
+print(is_perfect(81))
