5d71111ffeff7713442ae9ab9de953d2334bfd4f
erfan ashki <erfanbayern111@gmail.com>
2025-01-26 01:10:17 +0330

    implement House class and class attribute

--- python.py
+++ python.py
@@ -1,26 +1,29 @@
-class Car:
-    def __init__(self, car_color, model):
-        self.color = car_color
-        self.model = model
+class House():
+    country = 'iran'
+    
+    def __init__(self, num_of_room, color, area):
+        self.num_of_room = num_of_room
+        self.color = color
+        self.area = area
         
-    def turn_on_light(self):
-        print('Turn on')
-        
-    def rahnama_zadan(self, right_or_left):
-        return f"{right_or_left} is on"
+    def description_of_house(self):
+        return (self.num_of_room, self.color, self.area)     
 
-    def print_detail(self):
-        print(f"color is {self.color} - model is {self.model}")
         
-        
-my_car = Car('blue', 'BMW')
-print(my_car.color)
-print(my_car.model)
-my_car.turn_on_light()
-print(my_car.rahnama_zadan('right'))
+house1 = House(4, 'red', '300')
+house2 = House(3, 'white', '150')
 
-car1 = Car('black', '207')
-car1.print_detail()
-
-car1.print_detail(20)
- 
+print(house1.num_of_room)
+print(house2.num_of_room)
+print()
+print(house1.color)
+print(house2.color)
+print()
+print(house1.area)
+print(house2.area)
+print()
+print(house1.description_of_house())
+print(house2.description_of_house())
+print()
+print(house1.country)
+print(house2.country)
