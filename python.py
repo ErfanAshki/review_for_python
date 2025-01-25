6b2857b0074ce864a61f220ff0fadd6f5640bfe6
erfan ashki <erfanbayern111@gmail.com>
2025-01-26 00:51:17 +0330

    some change in Car class

--- python.py
+++ python.py
@@ -8,6 +8,9 @@ class Car:
         
     def rahnama_zadan(self, right_or_left):
         return f"{right_or_left} is on"
+
+    def print_detail(self):
+        print(f"color is {self.color} - model is {self.model}")
         
         
 my_car = Car('blue', 'BMW')
@@ -15,3 +18,9 @@ print(my_car.color)
 print(my_car.model)
 my_car.turn_on_light()
 print(my_car.rahnama_zadan('right'))
+
+car1 = Car('black', '207')
+car1.print_detail()
+
+car1.print_detail(20)
+ 
