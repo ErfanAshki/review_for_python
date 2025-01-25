1539f83d57cbd013729e727a979ce3d5923b44c0
erfan ashki <erfanbayern111@gmail.com>
2025-01-25 21:25:58 +0330

    implement Car class

--- python.py
+++ python.py
@@ -1,33 +1,17 @@
-def make_space (number_of_star , num) : 
-    number_of_space = int((num - number_of_star) / 2) 
-    print(number_of_space * ' ' , end= '') 
-    print(number_of_star * '*' , end= '') 
-    print(number_of_space * ' ')  
-
-
-
-
-
-def make_diamond (num) :
-    print()  
-    for i in range(1,num+1,2) :
-        make_space(i,num)   
-    
-    for i in range(1,num,2) :
-        make_space(num-i-1 , num)    
-
-    # for i in range (num) :
-    #     if i < num / 2 : 
-    #         print(i * 2 + 1) 
-    #     else : 
-    #         print((num - i) * 2 - 1 )  
-      
-user_number = int(input("Enter a number : ")) 
-
-if user_number == 0 : 
-    print('Invalid input , please enter a correct number')
-elif user_number % 2 == 0 : 
-    user_number += 1 
-
-
-make_diamond(user_number) 
+class Car:
+    def __init__(self, car_color, model):
+        self.color = car_color
+        self.model = model
+        
+    def turn_on_light(self):
+        print('Turn on')
+        
+    def rahnama_zadan(self, right_or_left):
+        return f"{right_or_left} is on"
+        
+        
+my_car = Car('blue', 'BMW')
+print(my_car.color)
+print(my_car.model)
+my_car.turn_on_light()
+print(my_car.rahnama_zadan('right'))
