bec25368e51d2a0e3e7ec1db45cca1232a804002
erfan ashki <erfanbayern111@gmail.com>
2025-01-26 15:22:53 +0330

    implement Circle class

--- python.py
+++ python.py
@@ -1,34 +1,27 @@
-class House():
-    country = 'iran'
+class Circle:
+    pi=3.14
     
-    def __init__(self, num_of_room, color, area):
-        self.num_of_room = num_of_room
-        self.color = color
-        self.area = area
+    def __init__ (self, r):
+        self.r = r
         
-    def description_of_house(self):
-        return (self.num_of_room, self.color, self.area)     
-
+    def calc_diameter(self):
+        return f"diameter : {self.r * 2}"
         
-house1 = House(4, 'red', '300')
-house2 = House(3, 'white', '150')
+    def calc_circumference(self):
+        return f"circumference : {2 * Circle.pi * self.r}"
+    
+    def calc_area(self):
+        return f"calc_area : {Circle.pi * self.r * self.r}"
+    
+    
+c1 = Circle(10)
+c2 = Circle(8)
 
-house1.num_of_room = 1
-house2.area = 100
-
-print(house1.num_of_room)
-print(house2.area)
-
-house1.country = 'usa'
-house2.country = 'germany'
-
-print(house1.country)
-print(house2.country)
-
-
-house1.x = 'xxx'
-house2.new_attr = 'attrrr'
-
-print(house1.x)
-print(house2.new_attr)
-
+print(c1.calc_diameter())
+print(c1.calc_circumference())
+print(c1.calc_area())
+print()
+print(c2.calc_diameter())
+print(c2.calc_circumference())
+print(c2.calc_area())
+print()
