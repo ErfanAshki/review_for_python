0194184d3780fbd46df3468d56d0b902c7c97b77
erfan ashki <erfanbayern111@gmail.com>
2025-01-26 16:46:39 +0330

    inheritance in classes part 1

--- python.py
+++ python.py
@@ -1,27 +1,15 @@
-class Circle:
-    pi=3.14
-    
-    def __init__ (self, r):
-        self.r = r
+class Person():
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
         
-    def calc_diameter(self):
-        return f"diameter : {self.r * 2}"
-        
-    def calc_circumference(self):
-        return f"circumference : {2 * Circle.pi * self.r}"
-    
-    def calc_area(self):
-        return f"calc_area : {Circle.pi * self.r * self.r}"
+    def fullname(self):
+        return f"{self.first_name}  {self.last_name}"
     
     
-c1 = Circle(10)
-c2 = Circle(8)
+class Student(Person):
+    pass
 
-print(c1.calc_diameter())
-print(c1.calc_circumference())
-print(c1.calc_area())
-print()
-print(c2.calc_diameter())
-print(c2.calc_circumference())
-print(c2.calc_area())
-print()
+
+ali = Student('ali', 'alavi')
+print(ali.fullname())
