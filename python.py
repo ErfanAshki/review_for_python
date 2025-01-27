d4dd29dbf81ecc5784308b5cd54f57e07420806a
erfan ashki <erfanbayern111@gmail.com>
2025-01-27 12:51:31 +0330

    use __len__ magic method

--- python.py
+++ python.py
@@ -5,10 +5,15 @@ class Square:
     def __str__(self):
         return f"square with {self.side_length} length ."
     
+    def __len__(self):
+        return self.side_length
+    
     
 s1 = Square(10)
 s2 = Square(5)
 
 print(s1)
 print(s2)
-
+print()
+print(len(s1))
+print(len(s2))
