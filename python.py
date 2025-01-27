16f2dbf390c9c0feb78d79d1dab9be337c5ef4a1
erfan ashki <erfanbayern111@gmail.com>
2025-01-27 18:11:55 +0330

    use __contains__ magic method part 2

--- python.py
+++ python.py
@@ -1,12 +1,7 @@
-class Contains:
-    def __init__(self, items):
-        self.items = items
-        
-    def __contains__(self, other):
-        return other in self.items
-    
-    
-a = Contains([1,2,3,4,5])
+sample = 'introspective'
+str = 'tro'
+strr = 'ttro'
 
-print(a.__contains__(7))
-print(a.__contains__(3))
+
+print(sample.__contains__(str))
+print(sample.__contains__(strr))
