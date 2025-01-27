92f53014a71a3d302f450656399b7ab246e81c18
erfan ashki <erfanbayern111@gmail.com>
2025-01-27 18:51:34 +0330

    use __iadd__ magic method

--- python.py
+++ python.py
@@ -9,8 +9,23 @@ class Deposit:
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self.name} , amount={self.amount})"
     
+    def __add__(self, other):
+        name = self.name + '|' + other.name
+        amount = self.amount + other.amount
+        return Deposit(name , amount)
+    
+    def __iadd__(self, other):
+        self.amount += other.amount
+        other.amount = 0
+        return self
+    
 
 john_dep = Deposit('john', 1000)
 david_dep = Deposit('david', 500)    
     
-print(repr(john_dep))
+print(john_dep + david_dep)
+
+john_dep += david_dep
+
+print(john_dep)
+print(david_dep)
