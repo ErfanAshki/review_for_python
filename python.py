dfe90713b638bb413fa3040c38ff4efec4849af4
erfan ashki <erfanbayern111@gmail.com>
2025-01-27 12:46:38 +0330

    use __str__ magic method

--- python.py
+++ python.py
@@ -1,35 +1,14 @@
-class Shape:
-    def __init__(self, kind, name):
-        self.kind = kind
-        self.name = name
-        
-    def area(self):
-        raise NotImplementedError("All shapes should be have own area method")
-    
-
-class Square(Shape):
-    def __init__(self, kind, name, side_length):
-        super().__init__(kind, name)
+class Square:
+    def __init__(self, side_length):
         self.side_length = side_length
         
-    def area(self):
-        return self.side_length * self.side_length
-        
-
-class Circle(Shape):
-    pi = 3.14
-
-    def __init__(self, kind, name, r):
-        super().__init__(kind, name)
-        self.r = r
-
-    def area(self):
-        return Circle.pi * self.r * self.r
+    def __str__(self):
+        return f"square with {self.side_length} length ."
     
-        
-sq = Square('square', 'first_sq', 10)
-ci = Circle('circle', 'first_ci', 10)
+    
+s1 = Square(10)
+s2 = Square(5)
 
-print(sq.area())
-print(ci.area())
+print(s1)
+print(s2)
 
