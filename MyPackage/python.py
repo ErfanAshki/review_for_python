54a4cc7bf428cf0dc193140fc330d8ec557ecb22
erfan ashki <erfanbayern111@gmail.com>
2025-01-27 22:09:07 +0330

    use __name__ & __main__

---
+++ MyPackage/python.py
@@ -0,0 +1,7 @@
+def some_function():
+    print('some func')
+
+if __name__ == "__main__":
+    print('This is python file')
+    print('hehe')
+
---
+++ python.py
@@ -0,0 +1,6 @@
+from MyPackage.python import some_function
+
+some_function()
+
+
+
