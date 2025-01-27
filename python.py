f54358c1fbde8d0a2fd55a7b90231b999c497254
erfan ashki <erfanbayern111@gmail.com>
2025-01-27 13:56:24 +0330

    implement Line class and calc_length & calc_slope methods

--- python.py
+++ python.py
@@ -1,19 +1,29 @@
-class Square:
-    def __init__(self, side_length):
-        self.side_length = side_length
+class Line:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
         
     def __str__(self):
-        return f"square with {self.side_length} length ."
-    
-    def __len__(self):
-        return self.side_length
-    
-    def __del__(self):
-        print (f"object with {self.side_length} length was deleted .")
+        return f"Line : a=({self.a[0]},{self.a[1]})  b=({self.b[0]},{self.b[1]})"
+        
+        
+    def calc_length(self):
+        return ((self.b[1] - self.a[1]) ** 2 + (self.b[0] - self.a[0]) ** 2) ** 0.5
     
     
-s1 = Square(10)
-s2 = Square(5)
+    def calc_slope(self):
+        return (self.b[1] - self.a[1]) / (self.b[0] - self.a[0])
+    
+    
+line1 = Line((1,1) , (3,3))
 
+print(line1)
+print(line1.calc_length())
+print(line1.calc_slope())
 
-print(s2)
+line2 = Line((4,9) , (8,1))
+
+print(line2)
+print(line2.calc_length())
+print(line2.calc_slope())
+
