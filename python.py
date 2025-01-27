2c20b3865a26a20fc65b2ccec5aa8c839154fc6d
erfan ashki <erfanbayern111@gmail.com>
2025-01-27 18:22:12 +0330

    implement Deposit class and use __repr__ magic method

--- python.py
+++ python.py
@@ -1,7 +1,16 @@
-sample = 'introspective'
-str = 'tro'
-strr = 'ttro'
+class Deposit:
+    def __init__(self, name, amount=0):
+        self.name = name
+        self.amount = amount
+        
+    def __str__(self):
+        return f"Owner : {self.name} | amount : {self.amount}"
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name} , amount={self.amount})"
+    
 
-
-print(sample.__contains__(str))
-print(sample.__contains__(strr))
+john_dep = Deposit('john', 1000)
+david_dep = Deposit('david', 500)    
+    
+print(repr(john_dep))
