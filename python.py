c5de93ae4d5eea240b56b27834bc37dfc7223cba
erfan ashki <erfanbayern111@gmail.com>
2025-01-27 15:00:57 +0330

    add if to Time class

--- python.py
+++ python.py
@@ -1,12 +1,22 @@
 class Time:
-    def __init__(self, hour, minute, second):
-        self.hour = hour
-        self.minute = minute
-        self.second = second
+    def __init__(self, hours, minutes, seconds):
+        if hours >= 24 : 
+            raise ValueError('Hour should be less than 24')
+        
+        if minutes > 59 : 
+            raise ValueError('Minute should be less than 60')
+        
+        if seconds > 59 :
+            raise ValueError('Second should be less than 60')
+        
+        
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
         
         
     def __str__(self):
-        return f"{self.hour:02}:{self.minute:02}:{self.second:02}"
+        return f"{self.hours:02}:{self.minutes:02}:{self.seconds:02}"
     
     
 my_time = Time(10, 25, 30)
