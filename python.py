daf5081d9ac72b3ad16e5a4c92b63c947463d462
erfan ashki <erfanbayern111@gmail.com>
2025-01-28 12:25:18 +0330

    use try except part 1

--- python.py
+++ python.py
@@ -1,4 +1,14 @@
-import mymoduile
+print('start')
 
+user_num = int(input('Enter a number : '))
 
-
+try:
+    print('first')
+    print(100/user_num)
+    print('second')
+    
+except:
+    print('except was occurred')
+    
+print('end')
+    
