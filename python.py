1bb87c4a3fb198e18d08f090aa077ab17dccf353
erfan ashki <erfanbayern111@gmail.com>
2025-01-28 15:07:56 +0330

    use assert with try except

--- python.py
+++ python.py
@@ -1,12 +1,12 @@
 def calc_income(income):
     if income <= 0 : 
-        raise Exception('Income must be bigger than 0')
+        assert income>0 , 'Income must be bigger than 0'
         
     return income * 2
 
 try:
     print(calc_income(-55))
-except:
+except Exception:
     print('Exception occurred')    
 
 print('end')
