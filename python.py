9c1887efe9626f7b5de68b911f2eaddb9552b2ed
erfan ashki <erfanbayern111@gmail.com>
2025-01-28 13:33:41 +0330

    use try except part 3 use different except

--- python.py
+++ python.py
@@ -1,13 +1,20 @@
 print('start')
 
 try:
-    import mymodule
-    print(100/user_num)
     print(10/0)
+    print(100/user_num)
+    import mymodule
     print('second')
     
-except:
-    print('except was occurred')
+except ModuleNotFoundError:
+    print('This module do not exist')
+    
+except NameError:
+    print('This variable do not define')
+    
+except ZeroDivisionError:
+    print('Numbers can not divide to zero')
     
 print('end')
+    
     
