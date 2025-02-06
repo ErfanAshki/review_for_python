6831b18d867d26b15c299e5242c62232ac29e547
erfan ashki <erfanbayern111@gmail.com>
2025-02-06 13:52:51 +0330

    pack()

--- python.py
+++ python.py
@@ -3,7 +3,10 @@ import tkinter as tk
 
 window = tk.Tk()
 
-text = tk.Label(master=window, text='hello world')
-text.pack()
+text1 = tk.Label(master=window, text='hello world')
+text2 = tk.Label(master=window, text='bye world')
+
+text1.pack()
+text2.pack()
 
 window.mainloop()
