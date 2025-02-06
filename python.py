b6841300feefcb1135489ff16d562cb0992ed752
erfan ashki <erfanbayern111@gmail.com>
2025-02-06 15:20:34 +0330

    use side in pack() - BOTTOM , TOP , RIGHT , LEFT

--- python.py
+++ python.py
@@ -8,7 +8,12 @@ text_var = tk.StringVar()
 entry = tk.Entry(master=window, textvariable=text_var)
 label = tk.Label(master=window, textvariable=text_var)
 
-entry.pack()
-label.pack()
+lb = tk.Label(master=window, text='hi')
+lbl = tk.Label(master=window, text='kha')
+
+entry.pack(side=tk.BOTTOM)
+label.pack(side=tk.TOP)
+lb.pack(side=tk.RIGHT)
+lbl.pack(side=tk.LEFT)
 
 window.mainloop()
