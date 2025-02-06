134c4d76fcd7a4bd1ee100e4d5106d30af521f19
erfan ashki <erfanbayern111@gmail.com>
2025-02-06 16:47:06 +0330

    columnspan

--- python.py
+++ python.py
@@ -3,15 +3,24 @@ import tkinter as tk
 
 window = tk.Tk()
 
+title = tk.Label(master=window, text='Enter your data')
+
 first_name_lbl = tk.Label(master=window, text='first_name : ')
 last_name_lbl = tk.Label(master=window, text='last_name : ')
 
 first_name_ent = tk.Entry(master=window)
 last_name_ent = tk.Entry(master=window)
 
-first_name_lbl.grid(row=0, column=0)
-first_name_ent.grid(row=0, column=1)
-last_name_lbl.grid(row=1, column=0)
-last_name_ent.grid(row=1, column=1)
+btn = tk.Button(master=window, text='submit')
+
+title.grid(row=0, column=0, columnspan=2)
+
+first_name_lbl.grid(row=1, column=0)
+first_name_ent.grid(row=1, column=1)
+last_name_lbl.grid(row=2, column=0)
+last_name_ent.grid(row=2, column=1)
+
+btn.grid(row=3, column=0, columnspan=2)
 
 window.mainloop()
+
