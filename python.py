3336b26c2e0034edc061dc7805a2f64b346f3f07
erfan ashki <erfanbayern111@gmail.com>
2025-02-07 00:22:45 +0330

    width & height with sticky ns

--- python.py
+++ python.py
@@ -19,11 +19,11 @@ btn = tk.Button(master=window, text='submit')
 title.grid(row=0, column=0, columnspan=2, sticky='ew')
 
 first_name_lbl.grid(row=1, column=0)
-first_name_ent.grid(row=1, column=1)
+first_name_ent.grid(row=1, column=1, sticky=(N,S))
 last_name_lbl.grid(row=2, column=0)
 last_name_ent.grid(row=2, column=1)
 age_lbl.grid(row=3, column=0, sticky=(W,))
-age_ent.grid(row=3, column=1)
+age_ent.grid(row=3, column=1, sticky='ns')
 
 btn.grid(row=4, column=0,columnspan=2, sticky=(W,E))
 
