da61ae8200f2f3c7eadaa6d1017947825d0b299d
erfan ashki <erfanbayern111@gmail.com>
2025-02-06 23:08:45 +0330

    sticky

--- python.py
+++ python.py
@@ -13,14 +13,14 @@ last_name_ent = tk.Entry(master=window)
 
 btn = tk.Button(master=window, text='submit')
 
-title.grid(row=0, column=0, columnspan=2)
+title.grid(row=0, column=0, columnspan=2, sticky='ew')
 
 first_name_lbl.grid(row=1, column=0)
 first_name_ent.grid(row=1, column=1)
 last_name_lbl.grid(row=2, column=0)
 last_name_ent.grid(row=2, column=1)
 
-btn.grid(row=3, column=0, columnspan=2)
+btn.grid(row=3, column=1, sticky='we')
 
 window.mainloop()
 
