100936f040626148315de1d3da4666e780696297
erfan ashki <erfanbayern111@gmail.com>
2025-02-09 12:40:59 +0330

    converter fahrenheit to celsius part 2 add padx & pady

--- python.py
+++ python.py
@@ -4,21 +4,22 @@ from tkinter import W, E, N, S
 
 window = tk.Tk()
 
-fah_lbl = tk.Label(master=window, text='fahrenheit')
-cel_lbl = tk.Label(master=window, text='celsius')
+fah_lbl = tk.Label(master=window, text='fahrenheit : ')
+cel_lbl = tk.Label(master=window, text='celsius : ')
 result_lbl = tk.Label(master=window, text='Result will be displayed here')
 
-fah_ent = tk.Entry(master=window)
+fah_ent = tk.Entry(master=window, width=30)
 
 calc_btn = tk.Button(master=window, text='calc')
 
-fah_lbl.grid(row=0, column=0)
-cel_lbl.grid(row=1, column=0)
-result_lbl.grid(row=1, column=1)
+fah_lbl.grid(row=0, column=0, padx=10, pady=(10,20))
+fah_ent.grid(row=0, column=1, pady=(10,20))
+calc_btn.grid(row=0, column=2, padx=10, pady=(10,20))
 
-fah_ent.grid(row=0, column=1)
+cel_lbl.grid(row=1, column=0, padx=10, pady=(5,20))
 
-calc_btn.grid(row=0, column=2)
+result_lbl.grid(row=1, column=1, pady=(5,20))
+
 
 
 window.mainloop()
