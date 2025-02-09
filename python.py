d94e5169f154097dcc2dedff90c3189833538dca
erfan ashki <erfanbayern111@gmail.com>
2025-02-09 14:16:32 +0330

    converter fahrenheit to celsius part 6 add bind to use enter

--- python.py
+++ python.py
@@ -12,7 +12,7 @@ result_lbl = tk.Label(master=window, text='Result will be displayed here')
 
 fah_ent = tk.Entry(master=window, width=30, textvariable=fahrenheit_val)
 
-def convert_fahrenheit_to_celsius():
+def convert_fahrenheit_to_celsius(*args):
     result = fah_ent.get()
     try:
         result = (float(result) - 32) * 5 / 9
@@ -30,6 +30,8 @@ calc_btn.grid(row=0, column=2, padx=10, pady=(10,20))
 cel_lbl.grid(row=1, column=0, padx=10, pady=(5,20))
 
 result_lbl.grid(row=1, column=1, pady=(5,20))
+
+window.bind('<Return>', convert_fahrenheit_to_celsius)
 
 window.title('Converting Temperature')
 
