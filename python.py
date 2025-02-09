42d4a2d0b6ce30a6ec2258d438e2541cef3322e9
erfan ashki <erfanbayern111@gmail.com>
2025-02-09 13:16:25 +0330

    converter fahrenheit to celsius part 4 implement convert_fahrenheit_to_celsius

--- python.py
+++ python.py
@@ -4,13 +4,21 @@ from tkinter import W, E, N, S
 
 window = tk.Tk()
 
+fahrenheit_val = tk.StringVar()
+
 fah_lbl = tk.Label(master=window, text='fahrenheit : ')
 cel_lbl = tk.Label(master=window, text='celsius : ')
 result_lbl = tk.Label(master=window, text='Result will be displayed here')
 
-fah_ent = tk.Entry(master=window, width=30)
+fah_ent = tk.Entry(master=window, width=30, textvariable=fahrenheit_val)
 
-calc_btn = tk.Button(master=window, text='calc')
+def convert_fahrenheit_to_celsius():
+    result = fah_ent.get()
+    result = (float(result) - 32) * 5 / 9
+    result_lbl['text'] = result
+    
+
+calc_btn = tk.Button(master=window, text='calc', width=10, command=convert_fahrenheit_to_celsius)
 
 fah_lbl.grid(row=0, column=0, padx=10, pady=(10,20))
 fah_ent.grid(row=0, column=1, pady=(10,20))
