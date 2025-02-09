78a8cf1af5654e09b97901545955e1b82dae24a9
erfan ashki <erfanbayern111@gmail.com>
2025-02-09 12:21:31 +0330

    converter fahrenheit to celsius part 1

--- python.py
+++ python.py
@@ -4,28 +4,22 @@ from tkinter import W, E, N, S
 
 window = tk.Tk()
 
-title = tk.Label(master=window, text='Enter your data')
+fah_lbl = tk.Label(master=window, text='fahrenheit')
+cel_lbl = tk.Label(master=window, text='celsius')
+result_lbl = tk.Label(master=window, text='Result will be displayed here')
 
-first_name_lbl = tk.Label(master=window, text='first_name : ',width=15, height=2)
-last_name_lbl = tk.Label(master=window, text='last_name : ',width=10, height=4)
-age_lbl = tk.Label(master=window, text='age : ',width=10, height=6)
+fah_ent = tk.Entry(master=window)
 
-first_name_ent = tk.Entry(master=window, width=10)
-last_name_ent = tk.Entry(master=window, width=20)
-age_ent = tk.Entry(master=window, width=30)
+calc_btn = tk.Button(master=window, text='calc')
 
-btn = tk.Button(master=window, text='submit')
+fah_lbl.grid(row=0, column=0)
+cel_lbl.grid(row=1, column=0)
+result_lbl.grid(row=1, column=1)
 
-title.grid(row=0, column=0, columnspan=2, sticky='ew')
+fah_ent.grid(row=0, column=1)
 
-first_name_lbl.grid(row=1, column=0)
-first_name_ent.grid(row=1, column=1, sticky=(N,S))
-last_name_lbl.grid(row=2, column=0)
-last_name_ent.grid(row=2, column=1)
-age_lbl.grid(row=3, column=0, sticky=(W,))
-age_ent.grid(row=3, column=1, sticky='ns')
+calc_btn.grid(row=0, column=2)
 
-btn.grid(row=4, column=0,columnspan=2, sticky=(W,E))
 
 window.mainloop()
 
