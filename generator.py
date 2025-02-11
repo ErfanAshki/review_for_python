31ad6d59556f97b1e455648b5594fccd5d7da8c5
erfan ashki <erfanbayern111@gmail.com>
2025-02-11 18:14:21 +0330

    use @wraps when create a decorator

--- generator.py
+++ generator.py
@@ -1,4 +1,7 @@
+from functools import wraps
+
 def my_decorator(input_func):
+    @wraps(input_func)
     def wrapper(*args, **kwargs):
         print('I use decorator')
         input_func(*args, **kwargs)
@@ -11,3 +14,4 @@ def sum_two_numbers(x,y):
 
 sum_two_numbers(4,5)
 
+print(sum_two_numbers)
