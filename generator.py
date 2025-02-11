332fd4c2a3dacb886c970278d330fd509dd694c8
erfan ashki <erfanbayern111@gmail.com>
2025-02-11 13:06:33 +0330

    give a function for input of another function

---
+++ generator.py
@@ -0,0 +1,11 @@
+def sum_two_numbers(x,y):
+    return x + y 
+
+
+def run_functions(your_func, a, b):
+    print('start')
+    print(your_func(a, b))
+    print('end')
+    
+run_functions(sum_two_numbers, 5, 7)
+
