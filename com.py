ee80aa9ce038b1bd206af8dce9410ca811c3c313
erfan ashki <erfanbayern111@gmail.com>
2025-02-13 17:00:23 +0330

    use list comprehension part 4 prime numbers

--- com.py
+++ com.py
@@ -1,4 +1,19 @@
+numbers = []
 
-numbers = [num ** 2  for num in range(15) if num ** 2 < 100]
+for nums in range(2,50):
+    prime = True
+    
+    for i in range(2,nums):
+        if nums % i == 0:
+            prime = False
+            break
+     
+    if prime:   
+        numbers.append(nums)
+    
 
 print(numbers)
+
+primes = [x for x in range(2,50) if all(x % y != 0 for y in range(2,x))]
+
+print(primes)
