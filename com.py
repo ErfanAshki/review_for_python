2e2505820f8c4b88dbefc18bcdc456bf3de3bb74
erfan ashki <erfanbayern111@gmail.com>
2025-02-21 12:53:16 +0330

    math module part 9 use radians

--- com.py
+++ com.py
@@ -1,6 +1,8 @@
 import math
 
 
-print(math.log2(25))
-print(math.log2(8600))
-print(math.log2(10))
+print(math.radians(120))
+print(math.radians(10))
+print(math.radians(45))
+print(math.radians(-63))
+print(math.radians(0))
