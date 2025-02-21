2ac545725a9fc8971478199064c395c37c6ee805
erfan ashki <erfanbayern111@gmail.com>
2025-02-21 15:14:43 +0330

    read a excel file with openpyxl module read each sheet

--- com.py
+++ com.py
@@ -3,7 +3,9 @@ from openpyxl import load_workbook
 workbook = load_workbook(filename='new_excel.xlsx')
 sheet = workbook.active
 
-for row in sheet.iter_rows(min_row=1, max_row=4, min_col=1, max_col=4):
-    for cell in row:
-        print(cell.value, end='  ')
+print(sheet['A1'].value)
+print(sheet['B1'].value)
+print(sheet['C2'].value)
+print(sheet['D1'].value)
+
 
