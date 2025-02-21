cebf32fede7c0c6576cbe469dcc56dd4fb1b2e7f
erfan ashki <erfanbayern111@gmail.com>
2025-02-21 12:05:15 +0330

    math module part 3 use fabs

--- com.py
+++ com.py
@@ -1,10 +1,9 @@
 import math
 
 
-print(math.floor(1.4))
-print(math.floor(-5.3))
-print(math.floor(22.6))
-print(math.floor(10))
-print(math.floor(5.2))
-print(math.floor(0.1))
+print(math.fabs(1.4))
+print(math.fabs(-5.3))
+print(math.fabs(22))
+print(math.fabs(-10))
+print(math.fabs(-0.1))
 
