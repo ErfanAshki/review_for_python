bc5907f1fd541a292089d8f4adbfd93931979eba
erfan ashki <erfanbayern111@gmail.com>
2025-02-21 15:20:51 +0330

    read a excel file with openpyxl module read just second column - just second row

--- com.py
+++ com.py
@@ -3,9 +3,13 @@ from openpyxl import load_workbook
 workbook = load_workbook(filename='new_excel.xlsx')
 sheet = workbook.active
 
-print(sheet['A1'].value)
-print(sheet['B1'].value)
-print(sheet['C2'].value)
-print(sheet['D1'].value)
+for i in range(1, sheet.max_column +1):
+    cell = sheet.cell(row=i, column=2)
+    print(cell.value, end='   ')
 
+print()
+
+for i in range(1, sheet.max_column +1):
+    cell = sheet.cell(row=2, column=i)
+    print(cell.value, end='   ')
 
