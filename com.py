1405279a5143a57b08f3bbdb309912915b2cda75
erfan ashki <erfanbayern111@gmail.com>
2025-02-21 11:57:48 +0330

    math module part 1 use ceil

--- com.py
+++ com.py
@@ -1,6 +1,10 @@
-list1 = ['word', 'calocation', 'definition']
-list2 = ['impasse', 'reach an', 'bon bast']
-list3 = ['guile', 'guide', 'neyrang']
+import math
 
-print(dict(zip(list1, list2)))
-print(dict(zip(list1, list3)))
+
+print(math.ceil(1.4))
+print(math.ceil(-5.3))
+print(math.ceil(22.6))
+print(math.ceil(10))
+print(math.ceil(5.2))
+print(math.ceil(0.1))
+
