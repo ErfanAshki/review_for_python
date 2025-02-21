ddbaa1883ebdf46d73278711dcd3466b7fb1e863
erfan ashki <erfanbayern111@gmail.com>
2025-02-21 15:03:19 +0330

    read a excel file with openpyxl module use cell

--- com.py
+++ com.py
@@ -4,5 +4,6 @@ workbook = load_workbook(filename='new_excel.xlsx')
 sheet = workbook.active
 
 for row in sheet.iter_rows(min_row=1, max_row=4, min_col=1, max_col=4):
-    print(row)
+    for cell in row:
+        print(cell)
 
