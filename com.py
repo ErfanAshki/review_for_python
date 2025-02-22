d08754370fb124e0b023af66fa1e76d5b1404278
erfan ashki <erfanbayern111@gmail.com>
2025-02-22 16:04:17 +0330

    use center method in string

--- com.py
+++ com.py
@@ -1,9 +1,5 @@
-import pickle
+print('penchant'.center(20))
+print('penchant'.center(10, 'k'))
+print('penchant'.center(20, '0'))
+print('penchant'.center(14, 'b'))
 
-names = None
-
-with open('name.txt', 'rb') as f:
-    names = pickle.load(f)
-    
-
-print(names)
